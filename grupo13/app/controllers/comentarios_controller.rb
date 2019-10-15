10f2e238b5ebfaeb7f1e04ffdbf7845ec3d4284d
Felipe Trejo <ftrejo@uc.cl>
2019-10-15 13:11:13 -0300

    Nuevo

--- grupo13/app/controllers/comentarios_controller.rb
+++ grupo13/app/controllers/comentarios_controller.rb
@@ -1,5 +1,6 @@
 class ComentariosController < ApplicationController
   before_action :set_comentario, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, :except => [:show, :index]
 
   # GET /comentarios
   # GET /comentarios.json
